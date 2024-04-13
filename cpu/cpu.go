d6de2e11912da7802f5f9f41f21c1f79f4024f8f
FV-Cano <cano.federicovicente@gmail.com>
2024-04-12 22:12:08 -0300

    Utils adaptadas a estructura de TP.
    Changelog:
    
    Nuevos paquetes:
    - cli
    - config
    - data-receive
    - data-send
    - log
    
    Cada uno tiene distribuidas las funciones utilizadas en el tp0.
    IniciarConfig cambió a ConfigInit, función polimórfica para (casi) cualquier tipo de struct de configuración
    
    Pendiente:
    - testear funcionalidad de casos restantes
    - agregar datos de tamaño dinámico a config (kernel)

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	cfg "github.com/sisoputnfrba/tp-golang/utils/config"
+)
+
+type T_CPU struct {
+	Port 				int 	`json:"port"`
+	IP_memory 			string 	`json:"ip_memory"`
+	Port_memory 		int 	`json:"port_memory"`
+	Number_felling_tlb 	int 	`json:"number_felling_tlb"`
+	Algorithm_tlb 		string 	`json:"algorithm_tlb"`
+}
+
+var configcpu T_CPU
+
+func main() {
+	err := cfg.ConfigInit("config_cpu.json", &configcpu)
+	if err != nil {
+		log.Fatalf("Error al cargar la configuracion %v", err)
+	}
+
+	log.Printf(("Algoritmo de reemplazo de TLB: %s"), configcpu.Algorithm_tlb)
 }
--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	cfg "github.com/sisoputnfrba/tp-golang/utils/config"
+)
+
+type T_ConfigKernel struct {
+	Port 				int 	`json:"port"`
+	IP_memory 			string 	`json:"ip_memory"`
+	Port_memory 		int 	`json:"port_memory"`
+	IP_cpu 				string 	`json:"ip_cpu"`
+	Port_cpu 			int 	`json:"port_cpu"`
+	Planning_algorithm 	string 	`json:"planning_algorithm"`
+	Quantum 			int 	`json:"quantum"`
+	Multiprogramming 	int 	`json:"multiprogramming"`
+}
+
+// Resources 			list `json:"resources"`
+// Resource_instances 	list `json:"resource_instances"`
+// TODO check list type
+
+var configkernel T_ConfigKernel
+
+func main() {
+    err := cfg.ConfigInit("config_kernel.json", &configkernel)
+	if err != nil {
+		log.Fatalf("Error al cargar la configuracion %v", err)
+	}
+
+	log.Printf(("Algoritmo de planificacion: %s"), configkernel.Planning_algorithm)
 }

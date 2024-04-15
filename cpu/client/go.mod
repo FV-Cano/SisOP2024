74cf735b95d5a0e1cbaff0f02ab3a1c1ac5549bc
CeciC24 <ccovalchuk@frba.utn.edu.ar>
2024-04-15 22:58:30 +0000

    Adaptamos el codigo para utilizar las nuevas utils

--- cpu/client/go.mod
+++
@@ -1,5 +0,0 @@
-module client
-
-require tp-2024-1c-GO-DieGO-GO-/utils v1.0.0
-replace tp-2024-1c-GO-DieGO-GO-/utils => ./tp-2024-1c-GO-DieGO-GO-/utils
-go 1.22
--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,43 +2,63 @@ package main
 
 import (
 	"log"
+	"net/http"
 
-	
 	cfg "github.com/sisoputnfrba/tp-golang/utils/config"
+	datareceive "github.com/sisoputnfrba/tp-golang/utils/data-receive"
 	datasend "github.com/sisoputnfrba/tp-golang/utils/data-send"
 )
 
 type T_CPU struct {
-	Port 				int 	`json:"port"`
-	IP_memory 			string 	`json:"ip_memory"`
-	Port_memory 		int 	`json:"port_memory"`
-	Number_felling_tlb 	int 	`json:"number_felling_tlb"`
-	Algorithm_tlb 		string 	`json:"algorithm_tlb"`
-	Message				string	`json:"Message"`
+	Port               int    `json:"port"`
+	IP_memory          string `json:"ip_memory"`
+	Port_memory        int    `json:"port_memory"`
+	Number_felling_tlb int    `json:"number_felling_tlb"`
+	Algorithm_tlb      string `json:"algorithm_tlb"`
+	Message            string `json:"message"`
 }
 
 var configcpu T_CPU
 
 func main() {
-	
-	err := cfg.ConfigInit("config_cpu.json", &configcpu)
-
+	// *** CONFIGURACION ***
+	err := cfg.ConfigInit("config-cpu.json", &configcpu)
 	if err != nil {
 		log.Fatalf("Error al cargar la configuracion %v", err)
 	}
+	// log.Printf(("Algoritmo de reemplazo de TLB: %s"), configcpu.Algorithm_tlb)
 
-	log.Printf(("Algoritmo de reemplazo de TLB: %s"), configcpu.Algorithm_tlb)
+	log.Println("Configuracion cargada")
 
-	// loggeamos el valor de la config
-	log.Println(configcpu.Message)
+	// ? loggeamos el valor de la config
+	// log.Println(configcpu.Message)
 
-	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+	// *** SERVIDOR ***
+	go inicializarServidor()
 
-	// enviar un mensaje al servidor con el valor de la config
-	//TODO PONER globals.ClientConfig.Port_memory
+	// *** CLIENTE ***
+	// ! ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+
+	//TODO PONER configcpu.Port_memory
+	log.Println("Enviando mensaje al servidor")
 	datasend.EnviarMensaje(configcpu.IP_memory, configcpu.Port, configcpu.Message)
-	
-	// generamos un paquete y lo enviamos al servidor
-	datasend.GenerarYEnviarPaquete(configcpu.IP_memory, configcpu.Port)
 
+	// Generamos un paquete y lo enviamos al servidor
+	// datasend.GenerarYEnviarPaquete(configcpu.IP_memory, configcpu.Port)
+
+	select {}
+}
+
+func inicializarServidor() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/paquetes", datareceive.RecibirPaquetes)
+	mux.HandleFunc("/mensaje", datareceive.RecibirMensaje)
+
+	log.Println("Servidor corriendo en el puerto 8003")
+
+	err := http.ListenAndServe(":8003", mux)
+	if err != nil {
+		panic(err)
+	}
 }

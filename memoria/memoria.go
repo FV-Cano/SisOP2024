b25c3826a6bdcba3ce050a9e16d7d020cc50d933
Mili-rulio <mrulio@frba.utn.edu.ar>
2024-04-26 12:02:01 -0300

    Leer instrucciones, guardar en lista y enviar.

--- memoria/memoria.go
+++ memoria/memoria.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	//"path/filepath"
 
 	cfg "github.com/sisoputnfrba/tp-golang/utils/config"
 	logger "github.com/sisoputnfrba/tp-golang/utils/log"
 	"github.com/sisoputnfrba/tp-golang/utils/server-Functions"
+
+	//"github.com/sisoputnfrba/tp-golang/utils/server-Functions"
+	//"github.com/sisoputnfrba/tp-golang/utils/slice"
+	"encoding/json"
+	"net/http"
+	"os"
 )
 
 type T_ConfigMemory struct {
@@ -29,11 +37,60 @@ func main() {
 		log.Fatalf("Error al cargar la configuracion %v", err)
 	}
 	log.Println("Configuracion MEMORIA cargada")
+	
 	// Handlers
-
 	// Iniciar servidor
 
-	go server.ServerStart(configmemory.Port)
-
+	log.Println("Instrucciones leídas por memoria.")
+	go server.ServerStart(configmemory.Port,RegisteredModuleRoutes())
+	log.Println("Instrucciones enviadas a CPU")	
+	
 	select {}
+	
+}
+
+func OpenFile(filePath string)(*os.File){
+	file, err := os.Open(filePath) //El paquete os provee el método ReadFile el cual recibe como argumento el nombre de un archivo el cual se encargará de leer. Al completar la lectura, retorna un slice de bytes, de forma que si se desea leer, tiene que ser convertido primero a una cadena de tipo string
+	if err != nil {
+			log.Fatal(err)
+		}
+		
+	return file
+}
+
+func  ReadInstructions(filePath string) []string {
+	
+	var instrucciones []string
+	//Lee linea por linea el archivo
+	scanner := bufio.NewScanner(OpenFile(filePath))
+    for scanner.Scan() {
+        // Agregar cada línea al slice de strings
+        instrucciones = append(instrucciones, scanner.Text())
+    }
+	if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+    return instrucciones
+}
+
+
+func ServerResponse(w http.ResponseWriter, r *http.Request) {
+
+	respuesta, err := json.Marshal(ReadInstructions(configmemory.Instructions_path))
+	if err != nil {
+		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(respuesta)
+}
+
+func RegisteredModuleRoutes() http.Handler {
+	moduleHandler := &server.ModuleHandler{
+		RouteHandlers: map[string]http.HandlerFunc{
+			"GET /serverresponse": ServerResponse,
+		},
+	}
+	return moduleHandler
 }

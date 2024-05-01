0f66438899eacc628a10366fa743fa027fbd1be4
Federico Vicente Cano <cano.federicovicente@gmail.com>
2024-04-30 21:53:26 -0300

    Funcionalidades pendientes de testeo
    Changelog:
    Manda y recibe PCB
    API folder (cpu) -> cpuapi.go
    Cambios globals.go -> cpu
    Cambios menores en Fetch

---
+++ cpu/API/cpuapi.go
@@ -0,0 +1,34 @@
+package cpu_api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/sisoputnfrba/tp-golang/utils/pcb"
+)
+
+func PCB_recv(w http.ResponseWriter, r *http.Request) {
+	var received_pcb pcb.T_PCB
+
+	// Decode PCB
+	err := json.NewDecoder(r.Body).Decode(&received_pcb)
+	if err != nil {
+		http.Error(w, "Failed to decode PCB", http.StatusBadRequest)
+		return
+	}
+	
+	fmt.Printf("Received PCB: %v\n", received_pcb) //Log?
+	// Sección donde trabajo el pcb recibido
+	
+	// Encode PCB
+	jsonResp, err := json.Marshal(received_pcb)
+	if err != nil {
+		http.Error((w), "Failed to encode PCB response", http.StatusInternalServerError)
+	}
+
+	// Send back PCB
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)	
+}
--- cpu/globals/globals.go
+++ cpu/globals/globals.go
@@ -1,11 +1,21 @@
-package cpu
+package globals
 
 import (
 	"log"
 	"time"
-
 )
 
+type T_CPU struct {
+	Port               	int    `json:"port"`
+	IP_memory          	string `json:"ip_memory"`
+	Port_memory        	int    `json:"port_memory"`
+	Number_felling_tlb 	int    `json:"number_felling_tlb"`
+	Algorithm_tlb      	string `json:"algorithm_tlb"`
+}
+
+var Configcpu *T_CPU
+
+// TODO: Mover a instrucciones
 func IO_GEN_SLEEP(cantidadUnidadesTrabajo int, cantTiempoDeTrabajo int) {
 	time.Sleep(time.Duration(cantTiempoDeTrabajo * cantidadUnidadesTrabajo))
 	log.Println("Se cumplio el tiempo de espera")

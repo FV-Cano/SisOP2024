0c5eb2f1170d72d7b788b3357b88b8edafa50f4f
VYEDID <vyedid@frba.utn.edu.ar>
2024-06-15 22:14:28 +0000

    TLB CON FIFO IS WORKINGG @milii

--- cpu/tlb/tlb.go
+++ cpu/tlb/tlb.go
@@ -13,13 +13,12 @@ type Pagina_marco struct {
 
 type TLB []map[int]Pagina_marco
 
-var CurrentTLB TLB = make(TLB)
+var CurrentTLB TLB
 var OrderedKeys []int //mantiene el orden de las claves en la TLB
 
-func BuscarEnTLB(pid int, pagina int) bool {
-
-	for _ := range CurrentTLB{
-		if _, exists := CurrentTLB[pid]; exists && CurrentTLB[pid].Pagina == pagina {
+func BuscarEnTLB(pid, pagina int) bool {
+	for _, entradaTLB := range CurrentTLB {
+		if entry, exists := entradaTLB[pid]; exists && entry.Pagina == pagina {
 			return true
 		}
 	}
@@ -27,13 +26,13 @@ func BuscarEnTLB(pid int, pagina int) bool {
 }
 
 func FrameEnTLB(pid int, pagina int) int {
-
-	if _, exists := CurrentTLB[pid]; exists && CurrentTLB[pid].Pagina == pagina {
-		ActualizarTLB(pid, pagina, CurrentTLB[pid].Marco)
-		return CurrentTLB[pid].Marco
+	for _, entradaTLB := range CurrentTLB {
+		if entry, exists := entradaTLB[pid]; exists && entry.Pagina == pagina {
+			ActualizarTLB(pid, pagina, entry.Marco)
+			return entry.Marco
+		}
 	}
 	return -1
-
 }
 
 func ObtenerPagina(direccionLogica int, nroPag int, tamanio int) int {
@@ -57,7 +56,7 @@ func CalcularDireccionFisica(frame int, offset int, tamanio int) int {
 
 }
 
-/*func ActualizarTLB(pid, pagina, marco int) { 
+/*func ActualizarTLB(pid, pagina, marco int) {
 	if globals.Configcpu.Algorithm_tlb == "FIFO" {
 	if len(tlb.CurrentTLB) >= globals.Configcpu.Number_felling_tlb {
 		// Si la TLB está llena, eliminar la entrada más antigua (FIFO)
@@ -71,27 +70,33 @@ func CalcularDireccionFisica(frame int, offset int, tamanio int) int {
 	}
 }*/
 
-func ActualizarTLB(pid, pagina, marco int) { 
+func ActualizarTLB(pid, pagina, marco int) {
 	switch globals.Configcpu.Algorithm_tlb {
-		case "FIFO":
-        if !BuscarEnTLB(pid, pagina) { //Si la página no está en la tlb
+	case "FIFO":
+		if !BuscarEnTLB(pid, pagina) { //Si la página no está en la tlb
 			if len(CurrentTLB) < globals.Configcpu.Number_felling_tlb {
-                nuevoElemento:= map[int]Pagina_marco{
-                    pid: {Pagina: pagina, Marco: marco},
-                }
-                CurrentTLB = append(CurrentTLB, nuevoElemento)
-            } else {
-                // Remover el primer elemento (FIFO) y agregar el nuevo
-                CurrentTLB = append(CurrentTLB[1:], map[int]Pagina_marco{
-                    pid: {Pagina: pagina, Marco: marco},
-                })
-				fmt.Println("LA TLB QUEDO ASI: ")
-				for i:= range CurrentTLB { 
-				fmt.Println("QUEDO ASIM" , CurrentTLB[i])
+				nuevoElemento := map[int]Pagina_marco{
+					pid: {Pagina: pagina, Marco: marco},
 				}
-        } 
-			
-		/*case "LRU":
+				CurrentTLB = append(CurrentTLB, nuevoElemento)
+				fmt.Printf("Se agregó la entrada %d a la TLB", CurrentTLB)
+				fmt.Println("LA TLB QUEDO ASI: ")
+				for i := range CurrentTLB {
+					fmt.Println(CurrentTLB[i])
+				}
+			} else {
+				// Remover el primer elemento (FIFO) y agregar el nuevo
+				CurrentTLB = append(CurrentTLB[1:], map[int]Pagina_marco{
+					pid: {Pagina: pagina, Marco: marco},
+				})
+				fmt.Printf("Se agregó la entrada %d a la TLB", CurrentTLB)
+				fmt.Println("LA TLB QUEDO ASI: ")
+				for i := range CurrentTLB {
+					fmt.Println(CurrentTLB[i])
+				}
+			}
+
+			/*case "LRU":
 			 if !BuscarEnTLB(pid, pagina) { //Si la página no está en la tlb
 				if len(CurrentTLB) < globals.Configcpu.Number_felling_tlb {
 					// Si la TLB no está llena, agregar la entrada
@@ -116,6 +121,6 @@ func ActualizarTLB(pid, pagina, marco int) {
 				}
 				CurrentTLB[pid] = Pagina_marco{Pagina: pagina, Marco: marco} // Agregar la nueva entrada
 				OrderedKeys = append(OrderedKeys, pid) // Agregar la nueva clave al final de la lista*/
-			}
+		}
 	}
 }
